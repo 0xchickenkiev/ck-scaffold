5914f41ab2fbdeb67cfef18ff5077d942a720aca
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-02-02 14:21:16 +0000

    Add the ability to sell tokens back to the vendor

--- challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -37,8 +37,22 @@ contract Vendor is Ownable {
   }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
-  //function sellTokens(uint265 _amount) {
-
-  //}
+  function sellTokens(uint256 _amount) public {
+    //Check amount selling is more than 0
+    require(_amount > 0, "No amount of tokens selected to sell");
+    //Check users token balance is enough to do swap
+    uint256 userBalance = yourToken.balanceOf(msg.sender);
+    require(userBalance >= _amount, "Not enough tokens in balance");
+    //Check vendor balance is enough for swap
+    uint256 amountOfEthToTransfer = _amount / tokensPerEth;
+    uint256 vendorBalance = address(this).balance;
+    require(vendorBalance >= amountOfEthToTransfer, "Vender doesn't have enough funds to sell");
+    //Send tokens from caller to address(this)
+    (bool sent) = yourToken.transferFrom(msg.sender, address(this), _amount);
+    require(sent, "Failed to transfer tokens");
+    //Send ETH from vendor to caller
+    (sent,) = msg.sender.call{value: amountOfEthToTransfer}("");
+    require(sent, "Failed to send ETH to user");
+  }
 
 }
