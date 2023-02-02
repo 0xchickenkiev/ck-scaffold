ada720179b91c98117d61d98ddf564f6c9e000fe
Kevin Edwards <kevin_edwards91@hotmail.com>
2023-02-02 12:12:39 +0000

    Add ability for the owner to withdraw ETH

--- challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,39 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
-  constructor(address tokenAddress) {
+  uint256 public constant tokensPerEth = 100;
+
+ constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    require(msg.value > 0, "Send Eth to buy some tokens");
+    uint256 buyAmount = msg.value * tokensPerEth;
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= buyAmount, "Not enough Eth in the vending machine");
+    (bool sent) = yourToken.transfer(msg.sender, buyAmount);
+    require(sent, "Failed to transfer token to user");
+    emit BuyTokens(msg.sender, msg.value, buyAmount);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+      uint256 ownerBalance = (address(this).balance);
+      require(ownerBalance > 0, "No balance to withdraw");
+      (bool sent,) = msg.sender.call{value: address(this).balance}("");
+      require(sent, "Failed to send balance to owner");
+  }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
+  //function sellTokens(uint265 _amount) {
+
+  //}
 
 }
